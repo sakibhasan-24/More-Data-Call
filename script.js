9ed9849bf3e7e4102b8a50e44a4ff9094f2c431e
sakibhasan-24 <sakibhasan7724@gmail.com>
2023-10-10 02:34:39 +0600

    front-end structure

---
+++ script.js
@@ -0,0 +1,8 @@
+// const syn = document.getElementById("synchronous");
+// setTimeout(() => {
+//   console.log("inside ");
+//   syn.innerText = "Thank! you are  not bocking me";
+// }, 6000);
+
+// console.log("yes i am running");
+// www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata
