41a6b405d5f0bd96c873e4774bcc954f198f0b46
sakibhasan-24 <sakibhasan7724@gmail.com>
2023-10-10 03:15:32 +0600

    XML-request

--- script.js
+++ script.js
@@ -6,3 +6,48 @@
 
 // console.log("yes i am running");
 // www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata
+const mealContainer = document.getElementById("meals-container");
+
+function getMeals(mealsName) {
+  const fristRequest = new XMLHttpRequest();
+  fristRequest.open(
+    "GET",
+    `https://www.themealdb.com/api/json/v1/1/search.php?s=${mealsName}`
+  );
+  fristRequest.send();
+  console.log(fristRequest.responseText);
+  fristRequest.addEventListener("load", function () {
+    //   console.log(fristRequest.responseText);
+    const data = JSON.parse(fristRequest.responseText);
+    console.log(data.meals[0]);
+    const div = document.createElement("div");
+    div.innerHTML = `
+        <div class="meals" id="meals">
+                <div class="image">
+                  <img
+                    src="${data.meals[0].strMealThumb}"
+                    alt=""
+                    srcset=""
+                  />
+                </div>
+                <div class="meals-info">
+                  <h1 class="title">${data.meals[0].strMeal}</h1>
+                  <h2 class="ingri">ingredients:${data.meals[0].strTags}</h2>
+                  <p>
+                    ${data.meals[0].strInstructions.slice(0, 200)}...
+                  </p>
+                  <h3 class="price">$100</h3>
+                  <button class="btn">Order Now</button>
+                </div>
+              </div>
+        `;
+    console.log(div);
+    mealContainer.appendChild(div);
+    mealContainer.style.opacity = 1;
+  });
+}
+// console.log(fristRequest);
+
+getMeals("Arrabiata");
+getMeals("Pasta");
+getMeals("Rice");
